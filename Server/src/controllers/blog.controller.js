3ccb7ae5d41ce06ebc5d986b44b6e996c00412b6
Satyanarayan Pattnayak <Pattnayaksatyanarayan77@gmail.com>
2025-04-02 05:53:28 +0530

    add blog-delete update

--- Server/src/controllers/blog.controller.js
+++ Server/src/controllers/blog.controller.js
@@ -16,8 +16,51 @@ const createBlog = asyncHandler(
         const result_create_query = await pool.query(create_query, [title, content, author]);
 
         res.status(201).json(new ApiResponse(201, [result_create_query.rowCount], "Blog Created Successfully,Thank you..!"));
-    })
+    });
+
+
+const deleteBlog = asyncHandler(
+    async (req, res) => {
+        const { blog_id } = req.body;
+        const author = req.user;
+
+        const delete_query = "DELETE FROM blog WHERE blog_id = $1 AND author = $2;"
+        const result_delete_query = await pool.query(delete_query, [blog_id, author]);
+
+        res.status(200).json(new ApiResponse(204, [result_delete_query.rowCount], "Blog Deleted Successfully..!"));
+    });
+
+
+const updateBlog = asyncHandler(
+    async (req, res) => {
+        const { title, content, blog_id } = req.body;
+        const author = req.user;
+
+        if (!title || !content) {
+            return res.status(200).json(new ApiError(400, "Please fill the required field.", "Validation Error"));
+        }
+
+        const update_query = " UPDATE blog SET title = $1, content = $2 WHERE blog_id = $3 AND author = $4;";
+        const result_update_query = await pool.query(update_query, [title, content, blog_id, author]);
+
+        res.status(200).json(new ApiResponse(200, [result_update_query.rowCount], "Blog Updated Successfully,Thank you..!"));
+    });
+
+
+const allBlog = asyncHandler(
+    async (req, res) => {
+        const { page, limit } = req.query;
+        
+    }
+);
 
 
 
-export { createBlog }
+
+
+export {
+    createBlog,
+    deleteBlog,
+    updateBlog,
+    allBlog
+}
--- Server/src/routers/blog.router.js
+++ Server/src/routers/blog.router.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
-import { createBlog } from "../controllers/blog.controller.js";
+import { allBlog, createBlog, deleteBlog, updateBlog } from "../controllers/blog.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js"
 
 const router = Router();
 
 
+//unsecured routes
+router.route("/retrieve/all").get(allBlog);
+
+
 //secured routes
 router.route("/create").post(verifyJWT, createBlog);
+router.route("/delete").delete(verifyJWT, deleteBlog);
+router.route("/update").patch(verifyJWT, updateBlog);
 
 
 
