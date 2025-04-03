7f30eddcf5c77c0b3b9f8eadef268e103cdfac52
Satyanarayan Pattnayak <Pattnayaksatyanarayan77@gmail.com>
2025-04-03 23:45:51 +0530

    add home: retrieve all blog api intigration

--- Client/src/components/BlogCards.jsx
+++ Client/src/components/BlogCards.jsx
@@ -1,71 +1,18 @@
 
-function BlogCards() {
+function BlogCards(props) {
     return (
         <>
-            <div className="BlogCards-container mx-5 mt-5">
-
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            </div>
-                            <div className="card-footer d-flex justify-content-between">
-                                <small className="text-body-secondary">Last updated 3 mins ago</small>
-                                <a href="#" className="card-link text-decoration-none">Read More..</a>
-                            </div>
-                        </div>
+            <div className="col">
+                <div className="card h-100">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.b_title}</h5>
+                        <p className="card-text">{props.b_content}</p>
                     </div>
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            </div>
-                            <div className="card-footer d-flex justify-content-between">
-                                <small className="text-body-secondary">Last updated 3 mins ago</small>
-                                <a href="#" className="card-link text-decoration-none">Read More..</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            </div>
-                            <div className="card-footer d-flex justify-content-between">
-                                <small className="text-body-secondary">Last updated 3 mins ago</small>
-                                <a href="#" className="card-link text-decoration-none">Read More..</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col">
-                        <div className="card h-100">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            </div>
-                            <div className="card-footer d-flex justify-content-between">
-                                <small className="text-body-secondary">Last updated 3 mins ago</small>
-                                <a href="#" className="card-link text-decoration-none">Read More..</a>
-                            </div>
-                        </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <small className="text-body-secondary">By {props.b_author}</small>
+                        <a className="card-link text-decoration-none">Read More..</a>
                     </div>
                 </div>
-
-                <nav className="mt-5" aria-label="...">
-                    <ul className="pagination d-flex justify-content-between">
-                        <li className="page-item disabled">
-                            <a className="page-link">&laquo; Previous</a>
-                        </li>
-                        <li className="page-item active">
-                            <a className="page-link" href="#">Next &raquo;</a>
-                        </li>
-                    </ul>
-                </nav>
-
             </div>
         </>
     )
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,12 +1,80 @@
+import { useEffect, useState } from "react";
 import BlogCards from "../components/BlogCards";
 import HeroHome from "../components/HeroHome";
+import Alert from "../components/Alert.jsx";
 import "../css/home.css"
+import api from "../utils/api.js";
 
 function Home() {
+
+    const limit = 3 * 4;
+    const [page, setPage] = useState(1);
+    const [total, setTotal] = useState(1);
+    const [response, setResponse] = useState([]);
+    const [error_msg, setError_msg] = useState(false);
+
+
+    useEffect(() => {
+
+        async function fetchTotal() {
+            try {
+                const res = await api.get("v1/blog/retrieve/all", {
+                    params: { page, limit }
+                });
+
+                setTotal(Math.max(res.data.data[1][0]?.total_blogs, 1));
+            } catch (error) {
+                console.error("Login error:", error);
+                setError_msg("Something went wrong. Try again!");
+            }
+        }
+
+        fetchTotal();
+    }, []);
+
+    useEffect(() => {
+
+        async function fetchData() {
+            try {
+                const res = await api.get("v1/blog/retrieve/all", {
+                    params: { page, limit }
+                });
+
+                setResponse(res.data.data[0]);
+            } catch (error) {
+                console.error("Login error:", error);
+                setError_msg("Something went wrong. Try again!");
+            }
+        }
+
+        fetchData();
+    }, [page]);
+
+
     return (
         <>
+            {error_msg && <Alert type="primary" message={error_msg} />}
             <HeroHome />
-            <BlogCards />
+
+            <div className="BlogCards-container mx-5 mt-5">
+                <div className="row row-cols-1 row-cols-md-3 g-4">
+                    {response.map((e) => (
+                        <BlogCards key={e.blog_id} b_id={e.blog_id} b_title={e.title} b_content={e.content} b_author={e.author_name} />
+                    ))
+                    }
+                </div >
+            </div>
+
+            <nav className="mt-5 mx-5" aria-label="...">
+                <ul className="pagination d-flex justify-content-between">
+                    <li className={`page-item ${page === 1 ? "disabled" : "active"}`}>
+                        <button className="page-link" onClick={() => setPage((prev) => Math.max(prev - 1, 1))} disabled={page === 1}>&laquo; Previous</button>
+                    </li>
+                    <li className={`page-item ${page === Math.ceil(total / limit) ? "disabled" : "active"}`}>
+                        <button className="page-link" onClick={() => setPage((prev) => Math.min(prev + 1, Math.ceil(total / limit)))} disabled={page === Math.ceil(total / limit)}>Next &raquo;</button>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
