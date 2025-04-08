db82b200f5d48deed348d37ffe86676eee8393a5
Satyanarayan Pattnayak <Pattnayaksatyanarayan77@gmail.com>
2025-04-09 04:31:22 +0530

    add create blog functionality

--- Client/src/components/CreateBlog.jsx
+++ Client/src/components/CreateBlog.jsx
@@ -1,15 +1,45 @@
+import { useState } from "react";
+import api from "../utils/api";
+import Alert from "./Alert";
 
 
 function CreateBlog() {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [message, setMessage] = useState("");
+    const [key, setKey] = useState(1);
+
+    async function submitHandler(e) {
+        e.preventDefault();
+
+        const data = { title, content };
+
+        try {
+            const res = await api.post("v1/blog/create", data);
+
+            setMessage(res.data.message);
+            setKey((prev) => prev + 1);
+        } catch (error) {
+            console.log("Erorr:", error);
+            setMessage("Something went wrong. Try again!");
+            setKey((prev) => prev + 1);
+        }
+
+        setTitle("");
+        setContent("");
+    }
+
     return (
         <>
+            {message && <Alert key={key} type="primary" message={message} />}
+
             <div className="container py-5 mt-5 CreateBlog-container">
                 <div className="row justify-content-center">
                     <div className="col-md-8 col-lg-6">
                         <div className="card shadow">
                             <div className="card-body p-4">
                                 <h3 className="mb-4 text-center CreateBlog-title">Write a Blog Post</h3>
-                                <form id="blogForm">
+                                <form id="blogForm" onSubmit={submitHandler}>
                                     <div className="mb-3">
                                         <label htmlFor="title" className="form-label fw-medium input-lable">Title</label>
                                         <input
@@ -19,6 +49,8 @@ function CreateBlog() {
                                             name="title"
                                             placeholder="Enter your blog title"
                                             required
+                                            onChange={(e) => setTitle(e.target.value)}
+                                            value={title}
                                         />
                                     </div>
                                     <div className="mb-3">
@@ -30,6 +62,8 @@ function CreateBlog() {
                                             rows="8"
                                             placeholder="Write your blog content here..."
                                             required
+                                            onChange={(e) => setContent(e.target.value)}
+                                            value={content}
                                         ></textarea>
                                     </div>
                                     <div className="d-grid">
